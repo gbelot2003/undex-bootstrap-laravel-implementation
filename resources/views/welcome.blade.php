8dcd1a365f20fc657f9860be8b7ef540f1c042c5
Gerardo Belot <gerardo.belot@gmail.com>
2019-10-29 15:15:32 -0600

    Adding fonts and fonts configurations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,7 @@
                                 </div>
                                 <div class="user-content">
                                     <h6>Ajoy Das</h6>
-                                    <span>Pro User</span>
+                                    <span>Pro User <i class="ti-wand"></i></span>
                                 </div>
                             </li>
                             <li class="active"><a href="index.html"><i class="icon_lifesaver"></i> <span>Dashboard</span></a></li>
